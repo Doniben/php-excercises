933912d38195014a6cef7a4249c40826b7ca3bb1
Doniben <doniben@esperanto.co>
2021-01-16 22:26:00 -0500

    [FEATURE] functions with verification calling from main class

---
+++ src/helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+if (! function_exists('upper')) {
+function upper($value)
+{
+    return Text\Format::upperText($value);
+}
+}
+
+if (! function_exists('upper')) {
+function lower($value)
+{
+    return Text\Format::lowerText($value);
+}
+}
+
+?>
+
