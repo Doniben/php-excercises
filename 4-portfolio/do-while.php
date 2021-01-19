a812ae5374462b0c87ff9699f75ed71c54ed6d05
Doniben <doniben@esperanto.co>
2021-01-18 19:40:36 -0500

    [FEATURE] do-while working

---
+++ 4-portfolio/do-while.php
@@ -0,0 +1,32 @@
+<?php
+$jobs = [
+    [
+      'position' => 'Python Developer',
+      'description' => 'Backend developer with Django and Django Rest',
+      'time' => 'From Junio/2021 to January 2021'
+    ],
+    [
+      'position' => 'JavaScript Developer',
+      'description' => 'Backend developer with Nodejs and Express',
+      'time' => 'From Junio/2019 to January 2020',
+    ],
+    [
+      'position' => 'PHP Developer',
+      'description' => 'Backend developer with PHP and Laravel',
+      'time' => 'From Junio/2018 to January 2019',
+    ]
+    ];
+$i = 0;
+do {
+    echo '<h5>' . $jobs[$i]['position'] . '</h5>';
+    echo '<p>' . $jobs[$i]['description'] . '</p>';
+    echo '<em>' . $jobs[$i]['time']. '</em>';
+    echo '<strong>Achievements:</strong>';
+    echo '<ul>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '</ul>';
+    $i = $i + 1;
+} while ($i < 3);
+?>
